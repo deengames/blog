b0ab0c4d93919ced73a8fe831ebd108f3094a79b
nightblade9 <nightbladecodes@gmail.com>
2018-04-04 12:58:30 -0400

    Use theme. Renamed theme folder.

--- temp/pelicanconf.py
+++ temp/pelicanconf.py
@@ -31,5 +31,7 @@ SOCIAL = (('You can add links in your config file', '#'),
 
 DEFAULT_PAGINATION = False
 
+THEME = 'dg-theme'
+
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
