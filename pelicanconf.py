c406ff8675b3a2bd35cfbe90741a65d115dc136b
nightblade9 <nightbladecodes@gmail.com>
2019-06-20 23:14:42 -0400

    TLD changes

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'nightblade'
 SITENAME = 'Deen Games Blog'
-SITEURL = 'https://deengames.com/blog'
+SITEURL = 'https://www.deengames.com/blog'
 
 PATH = 'content'
 
@@ -21,7 +21,7 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Deen Games', 'https://deengames.com'),
+LINKS = (('Deen Games', 'https://www.deengames.com'),
         ('Itch.io Page', 'https://deengames.itch.io/'),
         ('Patreon Page', 'https://patreon.com/deengames'),
         ('Discord', 'https://discord.gg/5D6XS3b'),
--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://deengames.com/blog'
+SITEURL = 'https://www.deengames.com/blog'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
